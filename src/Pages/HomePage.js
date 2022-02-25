d04e220b97e17613e20536190a26b4d957ce12f4
KateVlaar <vlaar@ualberta.ca>
2022-02-25 04:55:06 +0000

    update sample api

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import {Button} from '../Components/Button';
-import {Header} from '../Components/Header';
-import {KeywordBar} from '../Components/KeywordBar';
+import { Button } from '../Components/Button';
+import { Header } from '../Components/Header';
+import { KeywordBar } from '../Components/KeywordBar';
 
 export const HomePage = () => {
     const handleFormSubmit = () => {
-        fetch('http://localhost:4433/data/create', {
+        fetch('http://localhost:4433/filterpaper', {
             method: 'POST',
             body: JSON.stringify({
-                name: "callFlaskAPI"
+                keywords: ["age", "blood"]
             }),
             headers: {
                 "Content-type": "application/json; charset=UTF-8"
@@ -17,12 +17,12 @@ export const HomePage = () => {
             console.log(message);
         })
     }
-    
+
     return (
         <div>
             <Header></Header>
             <KeywordBar />
-            <div style={{paddingTop: '50px', display: 'block', textAlign: 'center'}}>
+            <div style={{ paddingTop: '50px', display: 'block', textAlign: 'center' }}>
                 <Button buttonText="Call Flask API" onClick={handleFormSubmit}></Button>
             </div>
         </div>
