af2def64ba304ee40f1302602b99d7671020f2d7
KateVlaar <vlaar@ualberta.ca>
2022-03-07 01:25:49 +0000

    login page ready to be connected to api

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button } from '../Components/Button';
 
 export const LoginPage = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPasswrod] = useState('');
+
+    const onLogin = () => {
+        fetch('http://localhost:4433/auth/login', {
+            method: 'POST',
+            body: JSON.stringify({
+                "username": username,
+                "password": password
+            }),
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            }
+        }).then(response => response.json()).then(message => {
+            // TODO: do something with the response
+            console.log(message);
+        });
+    }
+
     return (
         <div className='dark-background'>
             <div className='login-form-container'>
                 <p className="login-form-title">Data Mining System</p>
-                <input type="text" placeholder="Username" className="login-text-input"/>
-                <input type="text" placeholder="Password" className="login-text-input"/>
-                <div className='login-form-button'>
-                    <Button buttonText="Log in"></Button>
-                </div>
+                <form onSubmit={onLogin}>
+                    <input type="text" placeholder="Username" className="login-text-input" onInput={e => setUsername(e.target.value)} />
+                    <input type="password" placeholder="Password" className="login-text-input" onInput={e => setPasswrod(e.target.value)} />
+                    <div className='login-form-button'>
+                        <Button buttonText="Log in" onClick={(e) => onLogin(e)}></Button>
+                    </div>
+                </form>
                 <p className='request-account-button'>No account? Get started here</p>
             </div>
         </div>
