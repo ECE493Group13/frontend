6f11f69f9040c832fb7f7b1056dc7b8bdcbd6915
katevlaar <kate.vlaar@shaw.ca>
2022-03-20 16:30:10 -0600

    Models tab fetches list of models instead of using fake data

--- src/Components/Tabs/ModelsTab.js
+++ src/Components/Tabs/ModelsTab.js
@@ -1,55 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ModelListItem } from "../ModelListItem";
+import { API_BASE_URL } from "../../constants";
 
 export const ModelsTab = () => {
-  const fakeModelList = [
-    {
-      date: "02/22/2022",
-      title: "Back Pain",
-      hyperparameters: {
-        embedding_size: 200,
-        epochs_to_train: 15,
-        learning_rate: 0.025,
-        num_neg_samples: 25,
-        batch_size: 500,
-        concurrent_steps: 12,
-        window_size: 5,
-        min_count: 5,
-        subsample: 1e-3,
+  const [models, setModels] = useState([]);
+  const [fetchComplete, setFetchComplete] = useState(false);
+
+  useEffect(() => {
+    fetchDatasets();
+  }, []);
+
+  const fetchDatasets = () => {
+    const token = sessionStorage.getItem("token");
+
+    fetch(`${API_BASE_URL}/train-task`, {
+      method: "GET",
+      headers: {
+        Authorization: token,
+        "Content-type": "application/json; charset=UTF-8",
       },
-      taskCompleted: true,
-    },
-    {
-      date: "02/10/2022",
-      title: "Nausea",
-      hyperparameters: {
-        embedding_size: 200,
-        epochs_to_train: 15,
-        learning_rate: 0.025,
-        num_neg_samples: 25,
-        batch_size: 500,
-        concurrent_steps: 12,
-        window_size: 5,
-        min_count: 5,
-        subsample: 1e-3,
-      },
-      taskCompleted: false,
-    },
-  ];
+    })
+      .then((response) => {
+        setFetchComplete(true);
+        if (!response.ok) {
+          throw new Error("HTTP status " + response.status);
+        }
+        return response.json();
+      })
+      .then((json) => {
+        setModels(
+          json.filter((model) => {
+            return !model.is_error;
+          })
+        );
+      });
+  };
 
   return (
     <div className="second-tab">
-      {fakeModelList.map((model) => {
-        return (
-          <ModelListItem
-            key={model.title}
-            title={model.title}
-            hyperparameters={model.hyperparameters}
-            date={model.date}
-            showLoadingIndicator={!model.taskCompleted}
-          />
-        );
-      })}
+      {fetchComplete &&
+        models.map((model) => {
+          return (
+            <ModelListItem
+              key={model.id}
+              title={model.id}
+              hyperparameters={JSON.parse(model.hparams)}
+              date={model.created.split("T")[0]}
+              showLoadingIndicator={!model.is_complete}
+            />
+          );
+        })}
+      {fetchComplete && models.length === 0 && (
+        <p className="center-text">
+          You have no models. Train your datasets to generate a new model.
+        </p>
+      )}
     </div>
   );
 };
