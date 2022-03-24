65c628db7752ed8b4968621fe9ca0133e70e0335
KateVlaar <KateVlaar@users.noreply.github.com>
2022-03-24 22:25:21 +0000

    Prettified Code!

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,37 +1,34 @@
 // https://kubajz.dev/cypress-authentication-route2
-Cypress.Commands.add(
-  "login",
-  (path, visitOptions) => {
-    const options = {
+Cypress.Commands.add("login", (path, visitOptions) => {
+  const options = {
     method: "POST",
     url: "https://www.googleapis.com/oauth2/v4/token",
     body: {
-        client_id: Cypress.env("CLIENT_ID"),
-        client_secret: Cypress.env("CLIENT_SECRET"),
-        refresh_token: Cypress.env("REFRESH_TOKEN"),
-        grant_type: "refresh_token",
-        audience: Cypress.env("IAP_AUDIENCE"),
+      client_id: Cypress.env("CLIENT_ID"),
+      client_secret: Cypress.env("CLIENT_SECRET"),
+      refresh_token: Cypress.env("REFRESH_TOKEN"),
+      grant_type: "refresh_token",
+      audience: Cypress.env("IAP_AUDIENCE"),
     },
     // Restrict cypress from showing errored response by default.
     // It would dump the whole request object, including env values.
     failOnStatusCode: false,
-    };
-  
-    return cy.request(options).then((response) => {
+  };
+
+  return cy.request(options).then((response) => {
     if (response.status !== 200) {
-        throw new Error(
+      throw new Error(
         `Request to get auth token failed, response: ${JSON.stringify(
-            response.body,
-        )}`,
-        );
+          response.body
+        )}`
+      );
     }
 
     const { id_token: token } = response.body;
-    
+
     return cy.visit(path || "/", {
-        headers: { Authorization: `Bearer ${token}` },
-        ...visitOptions,
+      headers: { Authorization: `Bearer ${token}` },
+      ...visitOptions,
     });
-      });
-    },
-  );
+  });
+});
