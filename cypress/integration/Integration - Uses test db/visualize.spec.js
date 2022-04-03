43b16bee1c90534c4e0ee83388e911f56d421b7e
katevlaar <kate.vlaar@shaw.ca>
2022-04-03 14:46:44 -0600

    last integration tests

--- cypress/integration/Integration - Uses test db/visualize.spec.js
+++ cypress/integration/Integration - Uses test db/visualize.spec.js
@@ -4,25 +4,26 @@ describe("Visualize", () => {
 	const url = "http://localhost:3000";
 
 	beforeEach(() => {
-		cy.visit(url);
+    cy.visit(url);
 
-		// Login
-		cy.get("#input1").type(completeAccountUsername);
-		cy.get("#input2").type(completeAccountPassword);
-		cy.get(".dms-button").contains("Log in").click();
-	});
+    // Login
+    cy.get("#input1").type(completeAccountUsername);
+    cy.get("#input2").type(completeAccountPassword);
+    cy.get(".dms-button").contains("Log in").click();
+  });
 
-	it('should request plot data and recieve the correct arrays in the response', () => {
-		cy.get(".dms-tab-navigator > :nth-child(2)").click();
-		cy.get("#visualizeButton").click();
+  it('should request plot data and recieve the correct arrays in the response', () => {
+    cy.get(".dms-tab-navigator > :nth-child(2)").click();
+    cy.get("#visualizeButton").click();
 
-		cy.intercept('/visualize?train_task_id=52').as("plotData");
+    cy.intercept('/visualize?train_task_id=52').as("plotData");
 
-		cy.wait("@plotData").then((req) => {
+    cy.wait("@plotData").then((req) => {
       const body = JSON.parse(req.response.body);
+
       expect(Array.isArray(body.labels)).to.equal(true);
       expect(Array.isArray(body.x)).to.equal(true);
       expect(Array.isArray(body.y)).to.equal(true);
-		});
+    });
 	});
 });
