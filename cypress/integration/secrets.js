6e365775e920250ab96ecf8a5d611d9dcd0667df
Kate Vlaar <44733046+KateVlaar@users.noreply.github.com>
2022-04-06 22:48:49 -0600

    Delete secrets.js

--- cypress/integration/secrets.js
+++
@@ -1,5 +0,0 @@
-export const completeAccountUsername = "user5";
-export const completeAccountPassword = "password";
-export const completeAccountEmail = "user5@example.com";
-export const incompleteAccountUsername = "user7";
-export const incompleteAccountPassword = "password";
